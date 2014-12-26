524a5520ff1e4bff6151889b81401ee59bb5dfe8
Tom Broerse <tom.broerse@99designs.com>
2014-12-26 13:19:22 +1100

    Use new desktop notifications api

--- base-notifier.js
+++ base-notifier.js
@@ -7,7 +7,7 @@ YUI.add('base-notifier', function (Y) {
 
 	/**
 	 * @constructor
-	 */ 
+	 */
 	BaseNotifier = function () {
 		BaseNotifier.superclass.constructor.apply(this, arguments);
 	};
@@ -18,21 +18,21 @@ YUI.add('base-notifier', function (Y) {
 	// Attributes
 	//
 	BaseNotifier.ATTRS = {
-		
+
 		/**
 		 * The title of your extension
-		 */ 
+		 */
 		title : {
 			value : null
 		},
 
 		/**
 		 * The URL of the app
-		 */ 
+		 */
 		url : {
 			value : null
 		},
-		
+
 		/**
 		 * Known domains for the app. Domains here must be listed under
 		 * permissions in your manifest.json.
@@ -57,7 +57,7 @@ YUI.add('base-notifier', function (Y) {
 
 		/**
 		 * Icon to display while the user is logged in
-		 */ 
+		 */
 		icons : {
 			value : {
 				loggedIn : 'browser-action-icon-active.png',
@@ -68,22 +68,22 @@ YUI.add('base-notifier', function (Y) {
 
 		/**
 		 * The font color of the icon while logged in
-		 */ 
+		 */
 		loggedInColor : {
 			value : [246,101,2,200]
 		},
 
 		/**
 		 * The font color of the icon while logged out
-		 */ 
+		 */
 		notLoggedInColor : {
 			value : [105,105,105,200]
 		},
 
 		/**
-		 * The user's desktop notifications preference. True means they want 
+		 * The user's desktop notifications preference. True means they want
 		 * desktop notifications - False means they don't.
-		 */ 
+		 */
 		notificationPreference : {
 			getter : function () {
 				if (Y.Lang.isValue(localStorage.base_notifier_desktop_notifications)) {
@@ -100,33 +100,33 @@ YUI.add('base-notifier', function (Y) {
 		 * When this number increases, we'll send a desktop notification
 		 * and update the icon.
 		 * @type Int|Null
-		 */ 
+		 */
 		_number : null,
 
 		/**
 		 * Number of failed attempts to sign in to our app
 		 * the last successful one.
 		 * @type Int
-		 */ 
+		 */
 		_numFailedConnections : 0,
 
 		/**
 		 * Timer object (returned by Y.later) for the periodic checks.
 		 * (Call cancel on this to stop the checks)
 		 * @type Object
-		 */ 
+		 */
 		_timer : null,
 
 		/**
 		 * Whether or not the user is logged in to the app.
 		 * @type Boolean
-		 */ 
+		 */
 		_loggedIn: false,
 
 		/**
 		 * Initializes the notifier. Starts periodic checks and subscribes
 		 * the necessary event listeners
-		 */ 
+		 */
 		initializer : function () {
 
 			var me = this;
@@ -163,7 +163,7 @@ YUI.add('base-notifier', function (Y) {
 					}
 				}
 			});
-			
+
 			//
 			// Listen for messages from the content script
 			//
@@ -201,7 +201,7 @@ YUI.add('base-notifier', function (Y) {
 			});
 			chrome.tabs.executeScript(tabId, {
 				code: "(function () {" +
-					"var iId = setInterval(function () { " + 
+					"var iId = setInterval(function () { " +
 					"  if (YUI && top == self) {" +
 					"    clearInterval(iId);" +
 					"    YUI().use(['base-base', 'node-base', 'event-base'], function (Y) {" +
@@ -236,16 +236,16 @@ YUI.add('base-notifier', function (Y) {
 		},
 		/**
 		 * Draws the browserAction icon for this extension.
-		 * @param {String} txt 
-		 */ 
+		 * @param {String} txt
+		 */
 		drawIcon : function (txt) {
-			var 
+			var
 				canvas = document.getElementById('iconCanvas'),
 				context = canvas.getContext('2d'),
 				imageObj = new Image(),
 				iconURL = this._loggedIn ? this.get('icons').loggedIn : this.get('icons').notLoggedIn,
 				badgeColor = this._loggedIn ? this.get('loggedInColor') : this.get('notLoggedInColor');
-				
+
 			context.clearRect(0, 0, canvas.width, canvas.height);
 			imageObj.onload = function() {
 				context.drawImage(imageObj, 0, 0, 19, 19);
@@ -268,17 +268,17 @@ YUI.add('base-notifier', function (Y) {
 		 * an XHR request. Updates the icon and launches a notification if
 		 * needed / applicable.
 		 * @returns {Void}
-		 */ 
+		 */
 		fetchNumber : function (xhr) {
 			if (this.get('url')) {
 				if (Y.Lang.isNull(this._number)) {
 					this.drawIcon("...");
 				}
-				Y.io(this.get('url'), { 
+				Y.io(this.get('url'), {
 					on: {
 						success: this.onFetchNumberSuccess,
 						failure: this.onFetchNumberFailure
-					}, 
+					},
 					context : this
 				});
 			}
@@ -288,16 +288,16 @@ YUI.add('base-notifier', function (Y) {
 		 * Success callback for fetchNumber()
 		 * @param {Int} id YUI IO transaction ID
 		 * @param {Object} reponse YUI IO response object
-		 */ 
+		 */
 		onFetchNumberSuccess : function (id, response) {
-			var 
+			var
 				tmpNode = Y.Node.create(response.responseText),
 				newNumber = this.getNumberFromNode(tmpNode);
 
 			if (!this.drawNumberAndNotify(newNumber, true)) {
 				this.onFetchNumberFailure(id, response);
 			}
-			
+
 			//
 			// Clean up
 			//
@@ -307,9 +307,9 @@ YUI.add('base-notifier', function (Y) {
 		/**
 		 * Success callback for fetchNumber()
 		 * @param {Int} newNumber The new number of items to display
-		 * @param {Boolean} doNotify Whether or not to create a desktop notification 
+		 * @param {Boolean} doNotify Whether or not to create a desktop notification
 		 * @returns Boolean
-		 */ 
+		 */
 		drawNumberAndNotify : function (newNumber, doNotify) {
 			if (!Y.Lang.isNull(newNumber)) {
 				this._loggedIn = true;
@@ -338,9 +338,9 @@ YUI.add('base-notifier', function (Y) {
 		/**
 		 * Get the number (to display in the icon and notify about) from a DOM node.
 		 * The node is created from response text received by an XHR request on
-		 * the app url. 
+		 * the app url.
 		 * @parm {tmpNode}
-		 */ 
+		 */
 		getNumberFromNode : function (tmpNode) {
 			console.error('getNumberFromNode() must be overridden in your sub class');
 
@@ -355,7 +355,7 @@ YUI.add('base-notifier', function (Y) {
 		 * Failure callback for fetchNumber()
 		 * @param {Int} id YUI IO transaction ID
 		 * @param {Object} reponse YUI IO response object
-		 */ 
+		 */
 		onFetchNumberFailure : function (id, response) {
 			this._loggedIn = false;
 			this._numFailedConnections += 1;
@@ -370,45 +370,43 @@ YUI.add('base-notifier', function (Y) {
 
 		/**
 		 * Shows a desktop notification to notify the user of new email.
-		 */ 
+		 */
 		notify : function () {
 			var me = this;
 			if (this.get('notificationPreference')) {
-				if (this.notification) {
-					this.notification.cancel();
-				}
-				this.notification = webkitNotifications.createNotification(
-					this.get('icons').notification,  // icon url - can be relative
-					this.get('text').notificationTitle,  // notification title
-					Y.Lang.sub(this.get('text').success, { num : me._number.toString() })
-				);
-				this.notification.onclick = function () {
-					me.openApp(); 
-					this.cancel();
-				};
-				this.notification.show();
+				chrome.notifications.create(this.get('text').notificationTitle, {
+					type: "basic",
+  					title: this.get('text').notificationTitle,
+  					message: Y.Lang.sub(this.get('text').success, { num : me._number.toString() }),
+					iconUrl: this.get('icons').notification
+				}, function () {});
+				chrome.notifications.onClicked.addListener(function (notificationId) {
+					if (notificationId === me.get('text').notificationTitle) {
+						me.openApp();
+					}
+				});
 			}
 		},
 
 		/**
 		 * Opens the web app in new tab unless it's opened already in the
 		 * currently selected tab.
-		 */ 
+		 */
 		openApp : function () {
-			var 
+			var
 				me = this,
 				active = false,
 				dLen = me.get('domains').length;
 
 			Y.Array.map(
-				me.get('domains'), 
+				me.get('domains'),
 				function (e) {
 					var pattern = "*://*." + e + "/*";
 					chrome.tabs.query({
 						url : pattern
 					}, function (tabs) {
 						dLen = dLen-1;
-						var 
+						var
 							tabsLen = tabs.length,
 							i;
 						for (i=0; i < tabsLen; i++) {
